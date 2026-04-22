de7d24cf89f9729aa356741300d48a8a0083fbf7
Oscar Rodrigues <dikma.digital@grupodikma.com.br>
2026-04-22 17:54:49 -0300

    build vercel

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -2,29 +2,23 @@ import { PrismaClient } from "@prisma/client"
 import { PrismaPg } from "@prisma/adapter-pg"
 import { Pool } from "pg"
 
-const globalForPrisma = globalThis as unknown as {
+const connectionString = process.env.DATABASE_URL!
+
+const pool = new Pool({
+  connectionString,
+})
+
+const adapter = new PrismaPg(pool)
+
+const globalForPrisma = global as unknown as {
   prisma: PrismaClient | undefined
 }
 
 export const prisma =
   globalForPrisma.prisma ??
-  (() => {
-    const connectionString = process.env.DATABASE_URL
-
-    if (!connectionString) {
-      throw new Error("DATABASE_URL não definida")
-    }
-
-    const pool = new Pool({
-      connectionString,
-    })
-
-    const adapter = new PrismaPg(pool)
-
-    return new PrismaClient({
-      adapter,
-    })
-  })()
+  new PrismaClient({
+    adapter,
+  })
 
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma
