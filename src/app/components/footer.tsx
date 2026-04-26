c6748b57122b6c291c330039826b5935cbb87ab7
Oscar Notebook <oskharm12@gmail.com>
2026-04-26 11:46:44 -0300

    atualização

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -1,15 +1,14 @@
+import packageInfo from '../../../package.json'
 
-export function Footer (){
-    return(
-
-<footer className="flex-shrink-0 bg-white border-t border-gray-200">
-  <div className="px-[10%] h-20 py-4">
-    <div className="flex flex-col sm:flex-row items-center justify-between gap-2 text-sm text-gray-500">
-      <span>© 2026 ReDikma</span>
-      <span className="text-gray-500 sm:hidden">Versão 1.0.0</span>
-    </div>
-  </div>
-</footer>
-
-    )
+export function Footer() {
+  return (
+    <footer className="hidden md:block flex-shrink-0 bg-white border-t border-gray-200">
+      <div className="px-[10%] h-20 py-4">
+        <div className="flex flex-col sm:flex-row items-center justify-between gap-2 text-sm text-gray-500">
+          <span>© 2026 ReDikma</span>
+          <span className="text-gray-500">Versão {packageInfo.version}</span>
+        </div>
+      </div>
+    </footer>
+  );
 }
