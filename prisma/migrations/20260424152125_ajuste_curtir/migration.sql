94ec2ebe72dbe3f0a9ab94b92cd9b4855938aa03
Oscar <oscardikma@gmail.com>
2026-04-28 14:15:52 -0300

    teste

--- prisma/migrations/20260424152125_ajuste_curtir/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "Like" ADD COLUMN     "comentarioId" TEXT;
-
--- AddForeignKey
-ALTER TABLE "Like" ADD CONSTRAINT "Like_comentarioId_fkey" FOREIGN KEY ("comentarioId") REFERENCES "Comentario"("id") ON DELETE SET NULL ON UPDATE CASCADE;
